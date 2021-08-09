340401a1105e744588516ef7796b77659ecf0899
kruglovgg <kruglovgg@uralsibins.ru>
2021-08-09 13:30:55 +0300

    * first commit

---
+++ Broker/Broker.CUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Broker.CUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
